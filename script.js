1b4e42314098b9b3ed1d11da3dbc85c151103360
slownation <110495651+stephenfrancis1315@users.noreply.github.com>
2024-06-05 21:49:33 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function() {
-    const image = document.getElementById('scroll-image');
-    const scrollPosition = window.scrollY;
-    image.style.marginTop = scrollPosition * .20 + 'px'; // Adjust the factor to control the speed of movement
-});
